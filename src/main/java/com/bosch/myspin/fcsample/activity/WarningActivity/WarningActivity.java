c4bf6a02993145d4a1a60499edb708b72a26b2ba
US\BKA1FH <Aaron.Bonnell-Kangas@us.bosch.com>
2017-03-16 12:04:01 +0100

    another unhelpful commit note

--- src/main/java/com/bosch/myspin/fcsample/activity/WarningActivity/WarningActivity.java
+++ src/main/java/com/bosch/myspin/fcsample/activity/WarningActivity/WarningActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.bosch.myspin.fcsample.R;
@@ -17,6 +19,15 @@ public class WarningActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_warning);
+
+        // code for close button
+        Button closeButton = (Button) findViewById(R.id.bClose);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         // Check what message we got and update the string appropriately
         Intent intent = getIntent();
@@ -36,6 +47,8 @@ public class WarningActivity extends AppCompatActivity {
                 break;
         }
     }
+
+
 
     @Override
     protected void onResume() {
